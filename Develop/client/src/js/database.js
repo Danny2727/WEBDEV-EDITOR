ede7eea5400f0e3d998f02770a1398c71f86b5a3
Meech <meech@dannys-mbp.home>
2023-06-03 14:23:21 -0400

    removed line form database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -22,7 +22,7 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.put({ id: 1, jate: content });
+  const request = store.add({ text: content });
 
   const result = await request;
 
@@ -45,7 +45,7 @@ const request = store.getAll();
 
 const result = await request;
 console.log('result.value', result);
-return result
+
 
 }
 
