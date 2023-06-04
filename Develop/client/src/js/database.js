0b0acb02cc75fadf772c8a48e14e811ebaa26f7d
Meech <meech@dannys-mbp.home>
2023-06-03 21:25:57 -0400

    made corrections to database.js file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -41,7 +41,7 @@ const tx = jateDB.transaction('jate', 'readonly');
 
 const store = tx.objectStore('jate');
 
-const request = store.get(key);
+const request = store.get(1);
 
 const result = await request;
 console.log('result.value', result);
